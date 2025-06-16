b2938c2fa84870907cec885be0dff1e45bd22718
Alejandra-Alejandra <aljimjordan@gmail.com>
2025-06-16 13:37:37 +0100

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -214,6 +214,11 @@ window.newsTab = "news.global";
       dendryUI.dendryEngine._runActions(scene.onArrival);
       var displayContent = dendryUI.dendryEngine._makeDisplayContent(scene.content, true);
       $('#qualities').append(dendryUI.contentToHTML.convert(displayContent));
+      $('#news_content').empty();
+      var scene = dendryUI.game.scenes[window.newsTab];
+      dendryUI.dendryEngine._runActions(scene.onArrival);
+      var displayContent = dendryUI.dendryEngine._makeDisplayContent(scene.content, true);
+      $('#news_content').append(dendryUI.contentToHTML.convert(displayContent));
   };
 
   window.changeTab = function(newTab, tabId) {
